d5c67fd1c721e0c68d3a10b93889e728ffb9b232
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-04-27 07:47:48 -0300

    Refactor utils model

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -1,29 +1,33 @@
-from django.db import models 
+"""Django models utilities."""
 
-class CRidelModel(models.Model):
+# Django
+from django.db import models
+
+
+class CRideModel(models.Model):
     """Comparte Ride base model.
-
-    CRideModel acts as aan abstract base class from which every
+    CRideModel acts as an abstract base class from which every
     other model in the project will inherit. This class provides
     every table with the following attributes:
-        + created (DateTime): Store the datetime the object was created
-        + modified (DateTime): Store the datetime the object was modified. 
+        + created (DateTime): Store the datetime the object was created.
+        + modified (DateTime): Store the last datetime the object was modified.
     """
+
     created = models.DateTimeField(
         'created at',
         auto_now_add=True,
-        help_text='Date time on which the object was created.' 
+        help_text='Date time on which the object was created.'
     )
-
     modified = models.DateTimeField(
         'modified at',
-        auto_add=True,
-        help_text='Date time on which the object was last modified.' 
-    )        
+        auto_now=True,
+        help_text='Date time on which the object was last modified.'
+    )
 
     class Meta:
         """Meta option."""
+
         abstract = True
+
         get_latest_by = 'created'
         ordering = ['-created', '-modified']
-        
