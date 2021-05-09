885183c4d88d2cec517ad030c68e677bd4b2463f
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-05-09 04:48:21 -0300

    Add filters to circles views

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -10,6 +10,10 @@ from cride.circles.permissions.circles import IsCircleAdmin
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
 
+# Filters
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 # Models
 from cride.circles.models import Circle, Membership
 
@@ -23,6 +27,13 @@ class CircleViewSet(mixins.CreateModelMixin,
 
     serializer_class = CircleModelSerializer
     lookup_field = 'slug_name'  
+
+    # Filters
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('slug_name', 'name')
+    ordering_fields = ('rides_offered', 'rides_taken', 'name', 'created', 'member_limit')
+    ordering = ('-members__count', '-rides_offered', '-rides_taken')
+    filter_fields = ('verified', 'is_limited')
 
     def get_queryset(self):
         """Restrict list to public-only"""
